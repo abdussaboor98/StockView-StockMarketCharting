93de611ee962a10af5121e71baefa352cc91dbad
Abdus Saboor <36190643+abdussaboor98@users.noreply.github.com>
2020-01-23 17:25:38 +0530

    Angular initialised

--- StockView/src/app/app.component.ts
+++ StockView/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { User } from './models/users';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']F
 })
 export class AppComponent {
   title = 'StockView';
+  user = new User(101,'Abdus Saboor','gaffari98@gmail.com',9876543210);
+  main = true;
 }
