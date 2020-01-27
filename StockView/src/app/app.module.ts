f9e2835de653dce48ea526c6304441eb829bd50f
Abdus Saboor <36190643+abdussaboor98@users.noreply.github.com>
2020-01-27 17:53:10 +0530

    generated new components

--- StockView/src/app/app.module.ts
+++ StockView/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { SignupComponent } from './user/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { NewCompanyComponent } from './admin/new-company/new-company.component';
+import { NewExchangeComponent } from './admin/new-exchange/new-exchange.component';
+import { ImportExcelComponent } from './admin/import-excel/import-excel.component';
+import { NewIpoComponent } from './admin/new-ipo/new-ipo.component';
+import { AccountUpdateComponent } from './user/account-update/account-update.component';
 
 
 @NgModule({
@@ -14,7 +19,12 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     SignupComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewCompanyComponent,
+    NewExchangeComponent,
+    ImportExcelComponent,
+    NewIpoComponent,
+    AccountUpdateComponent
   ],
   imports: [
     BrowserModule,
